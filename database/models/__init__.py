6c9450e1a3d4c0ec24497cdabbb940696f21fc02
Oleg <ovpon@users.noreply.github.com>
2026-04-26 14:50:19 +0200

    Fix import: remove GRADE_TYPES from __init__

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -1,5 +1,5 @@
 from database.models.user import User
 from database.models.subject import Subject, DEFAULT_SUBJECTS
-from database.models.grade import Grade, GRADE_TYPES
+from database.models.grade import Grade
 
 __all__ = ["User", "Subject", "DEFAULT_SUBJECTS", "Grade", "GRADE_TYPES"]
